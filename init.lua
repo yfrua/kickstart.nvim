dc2658b40998eacf75990fecb668178d2b2144ea
Feirui Ye <56760758+yfrua@users.noreply.github.com>
2026-05-14 16:14:31 +0800

    disable mouse

--- init.lua
+++ init.lua
@@ -113,7 +113,9 @@ do
   vim.o.relativenumber = true
 
   -- Enable mouse mode, can be useful for resizing splits for example!
-  vim.o.mouse = 'a'
+  -- vim.o.mouse = 'a'
+  -- Disable mouse mode
+  vim.o.mouse = ''
 
   -- Don't show the mode, since it's already in the status line
   vim.o.showmode = false

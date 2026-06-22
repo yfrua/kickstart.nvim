b61ecc6311522f064311de86ee241fbef99555b4
Feirui Ye <feirui.ye@cern.ch>
2026-06-23 00:22:09 +0800

    fix: restrict clangd to C/C++ filetypes only

--- init.lua
+++ init.lua
@@ -714,6 +714,7 @@ do
   local servers = {
     clangd = {
       cmd = { 'clangd', '--background-index', '--clang-tidy' },
+      filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
       root_markers = { 'compile_commands.json', 'compile_flags.txt', '.clangd', '.git' },
       init_options = {
         fallbackFlags = { '-std=c++20' },
